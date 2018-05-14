cbd75e19ace58c48f85731377a5e36764df4aaec
Maciej Plusa <maciej.plusa@intive.com>
2018-05-14 17:57:19 +0200

    Reformat of regulations code

--- src/Components/Regulations/index.js
+++ src/Components/Regulations/index.js
@@ -1,54 +1,46 @@
-import './style.css';
 import React, { Component } from 'react';
-import {regulations} from '../../Resources/regulations.js';
-import Animate from 'react-smooth';
+import { regulations } from '../../Resources/regulations.js';
+
+import './style.css';
 
 export default class Regulations extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    componentDidMount() {
-        this.resizeTextRegular();
-    }
-
-    resizeTextRegular(){
-        let selector = document.getElementsByClassName("text-regulations");
-        for(let i = 0; i < selector.length; i++) {
-            if(selector[i].innerHTML.charAt(0) === "§") {
-                selector[i].style.fontSize="18px";
-                selector[i].style.fontWeight="bold";
-                selector[i].style.textAlign="center";
-                selector[i].style.marginTop="10px";
-            }
-            // eslint-disable-next-line
-            else if(parseInt(selector[i].innerHTML.charAt(0)) > 0) {
-                selector[i].style.marginLeft="15px";
-            }
-            else {
-                selector[i].style.marginLeft="25px";
-            }
-        }
-    }
-
     render() {
-        return(  
-                <div className={this.props.vissibleRegulation ? 
-                                "regulations"
-                                :"regulations-hide"} onClick={this.hideRegular}>
-                    <div className="regulations-background"/>
-                        <div className="regulations-table">
-                            <div className="regulations-title">REGULAMIN</div>
-                    <div className="regulations-regular-text">
-                    {regulations.map(function(reg, index){
-                        return <p key={index} className="text-regulations">{reg}</p>
-                    })}
+        return (
+            <div
+                className={[
+                    'regulations',
+                    this.props.vissibleRegulation ? '' : 'hide',
+                ].join(' ')}
+                onClick={this.hideRegular}
+            >
+                <div className="regulations-background" />
+                <div className="regulations-table">
+                    <h2 className="regulations-title">REGULAMIN</h2>
+                    <div className="regulations-contain">
+                        {regulations.map(function (content, index) {
+                            if (content.charAt(0) === '§') {
+                                return <h3 key={index}>{content}</h3>;
+                            } else if (parseInt(content.charAt(0), 10) > 0) {
+                                return <p key={index}>{content}</p>
+                            } else {
+                                return (
+                                    <p
+                                        key={index}
+                                        style={{
+                                            marginLeft: '10px',
+                                        }}
+                                    >
+                                        {content}
+                                    </p>
+                                );
+                            }
+                        })}
+                    </div>
+                    <div className="regulations-exit" onClick={this.props.hideRegulations}>
+                        <span>WYJŚCIE</span>
+                        <span className="glyphicon glyphicon-remove regulations-remove" />
+                    </div>
                 </div>
-                <div className="regulations-exit" onClick={this.props.hideRegulations}>
-                    <span>WYJŚCIE</span>   
-                    <span className="glyphicon glyphicon-remove regulations-remove"/>                
-                </div> 
-              </div>
             </div>
         )
     }
